a5ef912bd02a47bc323b456fe4fc0e413c4e3e29
cartman1978 <cartman_7802@yahoo.ie>
2021-06-01 09:23:39 +0000

    added fucntionality to update and delete records in the db

--- mongo_project.py
+++ mongo_project.py
@@ -5,7 +5,7 @@ if os.path.exists("env.py"):
 
 
 MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "myFirstDatabase"
+DATABASE = "myFirstDB"
 COLLECTION = "celebrities"
 
 
@@ -19,7 +19,7 @@ def mongo_connect(url):
 
 def show_menu():
     print("")
-    print("1. Add record")
+    print("1. Add a record")
     print("2. Find a record by name")
     print("3. Edit a record")
     print("4. Delete a record")
@@ -35,9 +35,9 @@ def get_record():
     last = input("Enter last name > ")
 
     try:
-        doc = coll.find({"first": first.lower(), "last": last.lower()})
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
     except:
-        print("Error accessing database")
+        print("Error accessing the database")
 
     if not doc:
         print("")
@@ -53,8 +53,8 @@ def add_record():
     dob = input("Enter date of birth > ")
     gender = input("Enter gender > ")
     hair_color = input("Enter hair color > ")
-    occupation = input("Enter occupation> ")
-    nationality = input("Enter nationality> ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
 
     new_doc = {
         "first": first.lower(),
@@ -74,17 +74,68 @@ def add_record():
         print("Error accessing the database")
 
 
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print("")
+            print("Document updated")
+        except:
+            print("Error accessing the database")
+
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == "y":
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Error accessing the database")
+        else:
+            print("Document not deleted")
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
             add_record()
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
@@ -96,4 +147,3 @@ def main_loop():
 conn = mongo_connect(MONGO_URI)
 coll = conn[DATABASE][COLLECTION]
 main_loop()
-
